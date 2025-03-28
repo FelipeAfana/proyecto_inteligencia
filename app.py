937ae5bfe98a9fd121e489415f2052e90af91aac
FelipeAfana <fafanador@unab.edu.co>
2025-03-28 16:05:43 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ import joblib
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-
-
 # Cargar modelo y scaler
-model = joblib.load("Knn.joblib")
+model = joblib.load("modelo_svm.joblib")
 scaler = joblib.load("scaler.joblib")
 
 # Función para escalar valores de entrada
@@ -24,14 +20,17 @@ st.markdown("# 🌍 Predicción de Calidad del Aire")
 st.sidebar.markdown("### 🏡 Ingrese los valores:")
 PM10 = st.sidebar.slider("PM10", 0.0, 300.0, 50.0)
 NO2 = st.sidebar.slider("NO2", 0.0, 200.0, 30.0)
+Humedad = st.sidebar.slider("Humedad (%)", 0.0, 100.0, 50.0)
 PM2_5 = st.sidebar.slider("PM2.5", 0.0, 200.0, 25.0)
 O3 = st.sidebar.slider("O3", 0.0, 300.0, 100.0)
+Lluvia = st.sidebar.slider("Lluvia (mm)", 0.0, 100.0, 5.0)
 Temperatura = st.sidebar.slider("Temperatura (°C)", -10.0, 50.0, 25.0)
-
+Vel_Viento = st.sidebar.slider("Vel. Viento (m/s)", 0.0, 20.0, 5.0)
+Dir_Viento = st.sidebar.slider("Dir. Viento (°)", 0.0, 360.0, 180.0)
 
 # Botón de predicción
 if st.sidebar.button("🔍 Predecir Calidad del Aire"):
-    input_values = np.array([PM10, NO2, PM2_5, O3, Temperatura])
+    input_values = np.array([PM10, NO2, Humedad, PM2_5, O3, Lluvia, Temperatura, Vel_Viento, Dir_Viento])
     input_scaled = scale_input(input_values)
     
     # Mostrar valores escalados
